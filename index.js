83e3cd70d8bb40abf5c67dbc17453f4ff7c00c67
Christopher Romero <romero.chrisprof@gmail.com>
2024-07-30 23:34:44 -0500

    added some psuedo code

---
+++ index.js
@@ -0,0 +1,27 @@
+// Going to be asking questions so import Inquirer
+const inquirer = require("inquirer");
+// Also going to need file creation
+const fs = require("fs");
+
+const questions = [
+    {
+        //characters
+    },
+    {
+        //text color
+    },
+    {
+        //shape
+    },
+    {
+        //shapes color
+    }
+
+]
+
+//After quiestions are filled
+// Create an SVG file named `logo.svg`
+// After a successful creation, insert "Generated logo.svg" into console
+//upon opening the svg file in a browser, there is a 200x200 image that matches the criteria entered
+
+//do an init call to start the application
