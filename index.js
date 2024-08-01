8f0eaad6acde1ea1aecdacc722fdb8f16a55230e
Christopher Romero <romero.chrisprof@gmail.com>
2024-08-01 11:30:27 -0500

    Added some more start code

--- index.js
+++ index.js
@@ -5,23 +5,52 @@ const fs = require("fs");
 
 const questions = [
     {
-        //characters
+        // Name of logo
+        type: "input",
+        name: 'name',
+        message: 'Enter The characters of the logo: '
     },
     {
         //text color
+        type: 'input',
+        name: 'text_color',
+        message: "What do you want the color of the text to be? "
     },
     {
         //shape
+        type: 'list',
+        name: 'shape',
+        choices: ['square', 'circle', 'triangle'],
+        message: 'Enter the format of your shape: '
     },
     {
         //shapes color
+        type: 'input',
+        name: 'shape_color',
+        message: 'What is the color of the shape: '
     }
 
 ]
 
 //After quiestions are filled
-// Create an SVG file named `logo.svg`
+// Create a function to write svg file
 // After a successful creation, insert "Generated logo.svg" into console
-//upon opening the svg file in a browser, there is a 200x200 image that matches the criteria entered
+function writeToFile(fileName, data){
+    fs.writeFile(fileName, generateSVG(data) , (err) => {
+        err ? console.error(err) : console.log("Generated logo.svg")
+    })
+}
+
+function init() {
+    inquirer
+        .prompt(questions)
+            .then((response) => {
+                const fileName = "logo.svg";
+                writeToFile(fileName, response);
+            })
+}
 
 //do an init call to start the application
+init();
+
+//upon opening the svg file in a browser, there is a 200x200 image that matches the criteria entered
