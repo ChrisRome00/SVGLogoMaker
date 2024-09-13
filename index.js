2aedafacc2ed4f8d44ba6aee4982357dce1f5c25
Christopher Romero <romero.chrisprof@gmail.com>
2024-09-13 11:10:09 -0500

    Created Front-end Auth, Queries, and Mutations

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const inquirer = require("inquirer");
 // Also going to need file creation
 const fs = require("fs");
 
+// Import classes
+const { Circle, Square, Triangle } = require('./lib/shapes.js')
+
 const questions = [
     {
         // Name of logo
@@ -31,6 +34,26 @@ const questions = [
     }
 
 ]
+
+function generateSVG(data) {
+    let shape;
+
+    if (data.shape == 'square') {
+      shape = new Square(data.shape_color)
+    }
+    else if (data.shape == 'circle') {
+      shape = new Circle(data.shape_color)
+    } else {
+      shape = new Triangle(data.shape_color)
+    }
+  
+    return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+  
+    ${shape.render()}
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${data.text_color}">${data.name}</text>
+  
+  </svg>`
+  }
 
 //After quiestions are filled
 // Create a function to write svg file
