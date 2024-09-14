730cd06c5e8e0ba987d6233c2d8d2209e7f71a0a
Christopher Romero <romero.chrisprof@gmail.com>
2024-09-14 16:16:35 -0500

    Finished Challenge

--- index.js
+++ index.js
@@ -47,13 +47,12 @@ function generateSVG(data) {
       shape = new Triangle(data.shape_color)
     }
   
-    return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
-  
-    ${shape.render()}
-    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${data.text_color}">${data.name}</text>
-  
-  </svg>`
-  }
+    return `
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+            ${shape.render()}
+            <text x="150" y="125" font-size="60" text-anchor="middle" fill="${data.text_color}">${data.name}</text>
+        </svg>`
+}
 
 //After quiestions are filled
 // Create a function to write svg file
